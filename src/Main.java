b1a05cde9c074d9a4094dea8e7666a53891291b2
mateusz pociecha <mateusz.pociecha@opoczta.pl>
2024-06-27 14:42:16 +0200

    poprawna wersja

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CarSimulatorGUI carSimulatorGUI = new CarSimulatorGUI();
+        carSimulatorGUI.setVisible(true);
+    }
+}
